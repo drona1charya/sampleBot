8189319c9207d925497bc6de936675c6ea9c60e0
drona1charya <pkpardeep480@gmail.com>
2016-12-18 15:01:00 +0530

    asf

--- index.php
+++ index.php
@@ -1,4 +1,45 @@
-<?php
+$challenge = $_REQUEST['hub_challenge'];
+$verify_token = $_REQUEST['hub_verify_token'];
 
-echo("499596817");
-?>
+if ($verify_token === 'dronfox') {
+  echo $challenge;
+}
+
+$input = json_decode(file_get_contents('php://input'), true);
+
+$sender = $input['entry'][0]['messaging'][0]['sender']['id'];
+$message = $input['entry'][0]['messaging'][0]['message']['text'];
+
+
+//API Url
+$url = 'https://graph.facebook.com/v2.6/me/messages?access_token=<my-token>';
+
+//Initiate cURL.
+$ch = curl_init($url);
+
+//The JSON data.
+$jsonData = '{
+    "recipient":{
+        "id":"'.$sender.'"
+    }, 
+    "message":{
+        "text":"Hey Lee!"
+    }
+}';
+
+//Encode the array into JSON.
+$jsonDataEncoded = $jsonData;
+
+//Tell cURL that we want to send a POST request.
+curl_setopt($ch, CURLOPT_POST, 1);
+
+//Attach our encoded JSON string to the POST fields.
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+
+//Set the content type to application/json
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+
+//Execute the request
+if(!empty($input['entry'][0]['messaging'][0]['message'])){
+$result = curl_exec($ch);
+}
